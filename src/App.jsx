6bcc2d7dd88066c64ed42d0df9a8fa3dc45f5afc
George <demina.ivann@gmail.com>
2025-01-27 11:29:48 -0800

    Retrieve data from api, cache it and use it

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,59 @@ import { useState } from "react";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
+import config from "./config.json";
 export default function App() {
-  const img = "/src/assets/someimg.jpg";
-  const alt = "if you see this, something went wrong";
-  const projectName = "APOD";
-  const title = "Aurora around Saturn's North Pole";
-  const date = "2024-12-9";
-  const description = `
-  lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet. 
-  lorem ipsum dolorem sit amet.lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet. 
-  lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.lorem ipsum dolorem sit amet. 
-  lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.lorem 
-  ipsum dolorem sit amet. lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet. lorem ipsum 
-  dolorem sit amet.lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet. lorem ipsum dolorem 
-  sit amet. lorem ipsum dolorem sit amet.lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.
-  lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.lorem ipsum dolorem sit amet.
-  lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.
-  lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.
-  lorem ipsum dolorem sit amet.lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.
-  lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.lorem ipsum dolorem sit amet.
-  lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.
-  lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.
-  lorem ipsum dolorem sit amet.lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.
-  lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.lorem ipsum dolorem sit amet.
-          lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.
-          lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.
-           lorem ipsum dolorem sit amet.lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.
-            lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.lorem ipsum dolorem sit amet.
-             lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.
-             lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.
-              lorem ipsum dolorem sit amet.lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.
-               lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.lorem ipsum dolorem sit amet.
-                lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.
-                lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.
-                 lorem ipsum dolorem sit amet.lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.
-                 lorem ipsum dolorem sit amet. lorem ipsum dolorem sit amet.lorem ipsum dolorem sit amet.`;
+  const [apodData, setApodData] = useState({
+    alt: "if you see this, something went wrong",
+    projectName: "APOD",
+    date: "",
+    img: "",
+    title: "",
+    description: "",
+  });
+  const fetchApodData = async () => {
+    let url = config.API_URL;
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+  };
+  const getApodData = async () => {
+    try {
+      let data;
+      let cachedData = sessionStorage.getItem("savedApodData");
+      let cacheExpiry =
+        JSON.parse(sessionStorage.getItem("savedApodDataExpiry")) ?? 0;
+      let cacheExpired = Date.now() > cacheExpiry;
+      if (cachedData && !cacheExpired) {
+        data = JSON.parse(cachedData);
+        console.log("Using cached data");
+      } else {
+        data = await fetchApodData();
+        console.log("Using fetched data");
+        cacheApodData(data);
+      }
+      return data;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const cacheApodData = (data) => {
+    const now = new Date().getTime();
+    let startOfDay = now - (now % 86400000);
+    let endDate = startOfDay + 86400000;
+    sessionStorage.setItem("savedApodData", JSON.stringify(data));
+    sessionStorage.setItem("savedApodDataExpiry", endDate);
+  };
+  useState(async () => {
+    let data = await getApodData();
+    setApodData((prevValues) => ({
+      ...prevValues,
+      date: data.date,
+      img: data.hdurl,
+      title: data.title,
+      description: data.explanation,
+    }));
+  }, []);
   // Sidebar opener/closer
   const [sidebarHidden, setSidebarHidden] = useState("hidden");
   const openSidebar = (e) => {
@@ -47,18 +65,18 @@ export default function App() {
   };
   return (
     <>
-      <Main img={img} alt={alt} />
+      <Main img={apodData.img} alt={apodData.alt} />
       <Sidebar
-        title={title}
-        date={date}
-        description={description}
+        title={apodData.title}
+        date={apodData.date}
+        description={apodData.description}
         sidebarHidden={sidebarHidden}
         closeSidebar={closeSidebar}
       />
       <Footer
-        title={projectName}
-        subtitle={title}
-        date={date}
+        title={apodData.projectName}
+        subtitle={apodData.title}
+        date={apodData.date}
         openSidebar={openSidebar}
       />
     </>
